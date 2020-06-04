a22b1bd3d353f71cba5952ef8074f204deea6711
joaoandrepestre <joaoandre.pestre@poli.ufrj.br>
2020-06-04 19:58:32 +0200

    Finishes setup menu, adds readme

--- src/visualization/Visualization.java
+++ src/visualization/Visualization.java
@@ -13,32 +13,38 @@ import processing.core.PVector;
  */
 public class Visualization extends PApplet {
 
-    public final int SIZE = 0;
-    public final int HOPS = 1;
-    public final int TOPOLOGY = 2;
-    public final int LLEADER = 3;
-    public final int LDELTA = 4;
-    public final int GLEADER = 5;
-    public final int GDELTA = 6;
+    // Possible states for the setup menu
+    public final int SIZE = 0; /* The menu is waiting for the input for the size of the network */
+    public final int HOPS = 1; /* The menu is waiting for the input for MAX_HOPS */
+    public final int TOPOLOGY = 2; /* The menu is waiting for the user to draw the topology */
+    public final int LLEADER = 3; /* The menu is waiting for the input for the local leaders */
+    public final int LDELTA = 4; /* The menu is waiting for the input for the local deltas */
+    public final int GLEADER = 5; /* The menu is waiting for the input for the global leader */
+    public final int GDELTA = 6; /* The menu is waiting for the input for the global deltas */
 
     public final ActorSystem system = ActorSystem.create("system"); /* Akka actor system */
 
     public Network net; /* The network to draw */
-    public PVector[] points;
-    public boolean recording = false;
-    public int screenshotCounter = 0;
-    public int recordingCounter = 0;
+    public PVector[] points; /* Geometrical positions of the nodes */
+    public boolean recording = false; /* Flag for recording a video of the simulation. 
+                                         While true, will save a sequence of frames of the simulation
+                                         in the directory ./recordings */
+    public boolean screenshooting = false;
+    public int screenshotCounter = 0; /* Counts how many screenshots were taken */
+    public int recordingCounter = 0; /* Counts how many recordings of the simulation were done */
     
-    public int menuState = SIZE;
-    public int highlightedNode = 0;
-    public String input = "";
-    public int networkSize;
-    public int[][] topologyGraph;
-    public int[] globalDeltas;
-    public int globalLeader = -1;
-    public int[] localDeltas;
-    public int[] localLeaders;
-    public int maxHops; 
+    public int menuState = SIZE; /* State of the menu */
+    public int highlightedNode = 0; /* Node being edited at the moment in the menu */
+    public String input = ""; /* Textual input used in the menu */
+    public int networkSize; /* Number of nodes in the network */
+    public int[][] topologyGraph; /* Graph representing the topology of the network.
+                                     If topologyGraph[i][j] is 1, the channel between 
+                                     nodes i and j is up */
+    public int[] globalDeltas; /* Initial array of distances to the global leader */
+    public int globalLeader = -1; /* Global leader of the network */
+    public int[] localDeltas; /* Array of initial distances to the local leaders of the network */
+    public int[] localLeaders; /* Array of local leaders of the network */
+    public int maxHops; /* Maximum number of hops between a node and its local leader */
 
     /*
      * Initial settings. Creates the window.
@@ -48,43 +54,17 @@ public class Visualization extends PApplet {
     }
 
     /*
-     * Initial setup. Creates the Akka system and the network.
-     */
-    public void setup() {
-        /* final ActorSystem system = ActorSystem.create("system"); /* Akka actor system
-
-        // Example 1: The graph we discussed in our meetings
-
-        int[][] topologyGraph = { { 0, 1, 1, 0, 0, 0 }, { 1, 0, 1, 1, 0, 0 }, { 1, 1, 0, 0, 1, 0 },
-                { 0, 1, 0, 0, 1, 0 }, { 0, 0, 1, 1, 0, 1 }, { 0, 0, 0, 0, 1, 0 } };
-        int[] globalDeltas = { 3, 3, 2, 2, 1, 0 };
-        int globalLeader = 5;
-        int[] localDeltas = { 1, 1, 0, 2, 1, 0 };
-        int[] localLeaders = { 2, 2, 2, 5, 5, 5 };
-
-        // Exemple 2: Simpler 3 node clique, 0 is the only leader
-        
-         int[][] topologyGraph = { { 0, 1, 1 }, { 1, 0, 1 }, { 1, 1, 0 } }; int[]
-         globalDeltas = { 0, 1, 1 }; int globalLeader = 0; int[] localDeltas = { 0, 1,
-         1 }; int[] localLeaders = { 0, 0, 0 };
-        
-
-        net = new Network(system, topologyGraph, globalDeltas, globalLeader, localDeltas, localLeaders);
-
-        float x, y, ang;
-        int n = topologyGraph.length;
-        float r = width / 5;
-        points = new PVector[n];
-        for (int i = 0; i < n; i++) {
-            ang = PI / 2 + i * (2 * PI / n);
-            x = width / 2 + r * cos(ang);
-            y = height / 2 - r * sin(ang);
-            points[i] = new PVector(x, y);
-        } */
-    }
-
-    /*
-     * Checks if the points (x3,y3) is between (x1,y1) and (x2,y2)
+     * Checks if the points p3 is between p1 and p2
+     * 
+     * @param p1 The first point
+     * 
+     * @param p2 The second point
+     * 
+     * @param p3 The point that might be in between
+     * 
+     * @return true if p3 is between p1 and p2
+     * 
+     * @return false otherwise
      */
     public boolean pointBetweenPoints(PVector p1, PVector p2, PVector p3) {
         float d = p1.dist(p3) + p3.dist(p2);
@@ -94,9 +74,18 @@ public class Visualization extends PApplet {
     }
 
     /*
-     * Draws an arrow from (x1,y1) to (x2,y2)
+     * Draws an arrow from  p1 to p2
+     * 
+     * @param p1 The first point
+     * 
+     * @param p2 The second point
      */
     public void drawArrow(PVector p1, PVector p2) {
+        if(pointBetweenPoints(p1, p2, new PVector(mouseX, mouseY))){
+            stroke(255,0,0);
+        }else{
+            stroke(0);
+        }
         line(p1.x, p1.y, p2.x, p2.y);
         pushMatrix();
         translate(p2.x, p2.y);
@@ -108,7 +97,11 @@ public class Visualization extends PApplet {
     }
 
     /*
-     * Draws an edge between the nodes at (x1, y1) and (x2, y2)
+     * Draws an edge between the nodes at p1 and p2
+     * 
+     * @param p1 Geometrical position of the highest node 
+     * 
+     * @param p2 Geometrical position of the lowest node
      */
     public void drawEdge(PVector p1, PVector p2) {
         float x1 = p1.x;
@@ -131,6 +124,8 @@ public class Visualization extends PApplet {
      * Draws the node in a position of the circumference defined by the nodeID.
      * Writes on the screen the nodes height and clock. Draws the edges from this
      * node to its neighbors of smaller height.
+     * 
+     * @param n The node to be drawn
      */
     public void drawNode(Node n) {
         int nodeColor = 255;
@@ -168,7 +163,7 @@ public class Visualization extends PApplet {
     }
 
     /*
-     * Draws every node
+     * Draws every node in the network
      */
     public void drawNetwork() {
         textSize(12);
@@ -185,16 +180,22 @@ public class Visualization extends PApplet {
      */
     public void keyPressed() {
         if(menuState==SIZE){
-            if(key == ENTER){
+            if(key == ENTER && !input.equals("")){
                 menuTransition();
+            }
+            else if(key == BACKSPACE){
+                input = input.substring(0, max(0, input.length()-1));
             }
             else if (Character.isDigit(key)){
                 input += key;
             }
         }
         else if(menuState==HOPS){
-            if(key == ENTER){
+            if(key == ENTER && !input.equals("")){
                 menuTransition();
+            }
+            else if(key == BACKSPACE){
+                input = input.substring(0, max(0, input.length()-1));
             }
             else if (Character.isDigit(key)){
                 input += key;
@@ -203,25 +204,23 @@ public class Visualization extends PApplet {
         else if(menuState==TOPOLOGY && key == ENTER){
             menuTransition();
         }
-        else if(menuState==LLEADER && key == ENTER){
+        else if(menuState==LLEADER && key == ENTER && isFull(localLeaders)){
             menuTransition();
         }
         else if(menuState == LDELTA){
             if(key == ENTER){
-                menuTransition();
-            }
-            else if(key == CODED){
-                if(keyCode == LEFT){
-                    localDeltas[highlightedNode] = Integer.parseInt(input);
+                if(!isFull(localDeltas) || !input.equals("")){
+                    if(!input.equals("")){
+                        localDeltas[highlightedNode] = Integer.parseInt(input);
+                    }
                     input = "";
                     highlightedNode = (highlightedNode+1)%networkSize;
+                }else{
+                    menuTransition();
                 }
-                else if(keyCode == RIGHT){
-                    localDeltas[highlightedNode] = Integer.parseInt(input);
-                    input = "";
-                    highlightedNode--;
-                    highlightedNode = highlightedNode<0?networkSize+highlightedNode:highlightedNode;
-                }
+            }
+            else if(key == BACKSPACE){
+                input = input.substring(0, max(0, input.length()-1));
             }
             else if(Character.isDigit(key)){
                 input += key;
@@ -229,20 +228,18 @@ public class Visualization extends PApplet {
         }
         else if(menuState == GDELTA){
             if(key == ENTER){
-                menuTransition();
-            }
-            else if(key == CODED){
-                if(keyCode == LEFT){
-                    globalDeltas[highlightedNode] = Integer.parseInt(input);
+                if(!isFull(globalDeltas) || !input.equals("")){
+                    if(!input.equals("")){
+                        globalDeltas[highlightedNode] = Integer.parseInt(input);
+                    }
                     input = "";
                     highlightedNode = (highlightedNode+1)%networkSize;
-                }
-                else if(keyCode == RIGHT){
-                    globalDeltas[highlightedNode] = Integer.parseInt(input);
-                    input = "";
-                    highlightedNode--;
-                    highlightedNode = highlightedNode<0?networkSize+highlightedNode:highlightedNode;
-                }
+                }else{
+                    menuTransition();
+                }            
+            }
+            else if(key == BACKSPACE){
+                input = input.substring(0, max(0, input.length()-1));
             }
             else if(Character.isDigit(key)){
                 input += key;
@@ -250,10 +247,7 @@ public class Visualization extends PApplet {
         }
 
         if (key == 'p' || key == 'P') {
-            save("screenshots/simulation" + screenshotCounter + ".png");
-            fill(255);
-            rect(0,0,width,height);
-            screenshotCounter++;
+            screenshooting = true;
         }
         if (key == 'r' || key == 'R') {
             recording = !recording;
@@ -262,6 +256,13 @@ public class Visualization extends PApplet {
         }
     }
 
+    /* 
+     * Checks if the mouse is over a possible edge of the network.
+     * 
+     * @return The pair of id's of the nodes that form the edge qhere the mouse is
+     * 
+     * @return null if the mouse is not over an edge
+     */
     public PVector mouseOverEdge(){
         PVector mouse = new PVector(mouseX, mouseY);
         PVector p1;
@@ -280,6 +281,13 @@ public class Visualization extends PApplet {
         return null;
     }
 
+    /*
+     * Checks if the mouse is over a node
+     * 
+     * @return The id of the node the mouse is over
+     * 
+     * @return -1 if the mouse is not over any node 
+     */
     public int mouseOverNode(){
         PVector mouse = new PVector(mouseX, mouseY);
         PVector p;
@@ -292,6 +300,9 @@ public class Visualization extends PApplet {
         return -1;
     }
 
+    /* 
+     * Mouse pressed handler
+     */
     public void mousePressed() {
         PVector ij;
         int n;
@@ -304,13 +315,13 @@ public class Visualization extends PApplet {
             }
             else if(menuState > GDELTA){
                 if (mouseButton == LEFT){
-                    topologyGraph[i][j] = 1 - topologyGraph[i][j]; 
-                    topologyGraph[j][i] = 1 - topologyGraph[j][i];
                     if(topologyGraph[i][j] == 1){
                         net.dropChannel(i, j);
                     } else{
                         net.remakeChannel(i,j);
                     }
+                    topologyGraph[i][j] = 1 - topologyGraph[i][j]; 
+                    topologyGraph[j][i] = 1 - topologyGraph[j][i];
                 }
             }
         }
@@ -326,6 +337,25 @@ public class Visualization extends PApplet {
         }
     }
 
+    /* 
+     * Checks if the array's positions are not negative
+     * 
+     * @param array The array to be checked
+     * 
+     * @return true if the array is full
+     * 
+     * @return false otherwise
+     */
+    public boolean isFull(int[] array){
+        for(int i: array){
+            if(i<0) return false;
+        }
+        return true;
+    }
+
+    /* 
+     * Changes the state of the menu depending on the current state
+     */
     public void menuTransition(){
         switch (menuState) {
             case SIZE:
@@ -381,6 +411,15 @@ public class Visualization extends PApplet {
         }
     }
 
+    /* 
+     * Checks if the node is a local leader
+     * 
+     * @param i The id of the node to be checked
+     * 
+     * @return true if the node is in the local leaders array
+     * 
+     * @return false otherwise
+     */
     public boolean nodeIsLocalLeader(int i){
         for(int leader: localLeaders){
             if(leader == i){
@@ -390,6 +429,9 @@ public class Visualization extends PApplet {
         return false;
     }
 
+    /* 
+     * Draws the current state of the start menu
+     */
     public void startMenu(){
         PVector p1, p2;
         if(menuState==SIZE){
@@ -438,48 +480,80 @@ public class Visualization extends PApplet {
                 fill(nodeColor);
                 if((menuState == LLEADER || menuState == LDELTA || menuState == GDELTA) && i==highlightedNode){
                     fill(0,255,0);
+                    textColor = 0;
                 }
                 ellipse(p1.x, p1.y, 20, 20);
                 fill(textColor);
                 textAlign(CENTER, CENTER);
                 textSize(12);
                 text(i, p1.x, p1.y);
-                if((menuState == LDELTA || menuState == GDELTA) && i==highlightedNode){
+                if(menuState == LLEADER || menuState == LDELTA || menuState == GDELTA){
+                    String s = "";
+                    if(i==highlightedNode && !input.equals("")){
+                        s = input;
+                    } else if(menuState == LDELTA && localDeltas[i]>=0){
+                        s += localDeltas[i];
+                    } else if(menuState == GDELTA && globalDeltas[i]>=0){
+                        s += globalDeltas[i];
+                    } else if(menuState == LLEADER && localLeaders[i]>=0){
+                        s += localLeaders[i];
+                    }
                     if (p1.x >= width / 2) {
                         textAlign(LEFT, CENTER);
                         fill(0);
-                        text(input, p1.x + 15, p1.y);
+                        text(s, p1.x + 15, p1.y);
                     } else {
                         textAlign(RIGHT, CENTER);
                         fill(0);
-                        text(input, p1.x - 15, p1.y);
+                        text(s, p1.x - 15, p1.y);
                     }
                 }
             }
             if(menuState==TOPOLOGY){
+                fill(0);
                 textSize(20);
                 textAlign(CENTER, CENTER);
                 text("Use your mouse to draw the topology.\nPress ENTER when done", width/2, height/10);
             }
             else if(menuState==LLEADER){
+                fill(0);
                 textSize(20);
                 textAlign(CENTER, CENTER);
-                text("Click on the local leader of the highlighted node.\nPress ENTER when done", width/2, height/10);
+                if(!isFull(localLeaders)){
+                    text("Click on the local leader of the highlighted node.", width/2, height/10);
+                }else{
+                    text("Edit the local leader of the highlighted node.\nPress ENTER if done", width/2, height/10);
+                }
             }
             else if(menuState==LDELTA){
+                fill(0);
                 textSize(20);
                 textAlign(CENTER, CENTER);
-                text("Enter the local delta of the highlighted node.\nNavigate using <- and ->. Press ENTER when done", width/2, height/10);
+                if(!isFull(localDeltas)){
+                    text("Enter the local delta of the highlighted node.\nPress ENTER to go to next node", width/2, height/10);
+                }else if (!input.equals("")){
+                    text("Edit the local delta of the highlighted node.\nPress ENTER to go to next node", width/2, height/10);
+                }else{
+                    text("Edit the local delta of the highlighted node.\nPress ENTER if done", width/2, height/10);
+                }
             }
             else if(menuState == GLEADER){
+                fill(0);
                 textSize(20);
                 textAlign(CENTER, CENTER);
                 text("Click on the global leader.", width/2, height/10);
             }
             else if(menuState == GDELTA){
+                fill(0);
                 textSize(20);
                 textAlign(CENTER, CENTER);
-                text("Enter the global delta of the highlighted node.\nNavigate using <- and ->. Press ENTER when done", width/2, height/10);
+                if(!isFull(globalDeltas)){
+                    text("Enter the global delta of the highlighted node.\nPress ENTER to go to next node", width/2, height/10);
+                }else if (!input.equals("")){
+                    text("Edit the global delta of the highlighted node.\nPress ENTER to go to next node", width/2, height/10);
+                }else{
+                    text("Edit the global delta of the highlighted node.\nPress ENTER if done", width/2, height/10);
+                }
             }
         } 
     }
@@ -519,16 +593,18 @@ public class Visualization extends PApplet {
                 line(p1.x,p1.y,p2.x,p2.y);
             }
             drawNetwork();
-            if(edge != null && topologyGraph[(int) edge.x][(int) edge.y]==1){
-                PVector p1 = points[(int) edge.x];
-                PVector p2 = points[(int) edge.y];
-                stroke(255,0,0,85); 
-                line(p1.x,p1.y,p2.x,p2.y);
-            }
+        }
+
+        if(screenshooting){
+            screenshooting = false;
+            save("../screenshots/simulation" + screenshotCounter + ".png");
+            fill(255);
+            rect(0,0,width,height);
+            screenshotCounter++;
         }
         
         if (recording) {
-            saveFrame("recordings/simulation" + recordingCounter + "-######.png");
+            saveFrame("../recordings/simulation" + recordingCounter + "-######.png");
             fill(255, 0, 0);
             ellipse(width - 20, 20, 15, 15);
         }
